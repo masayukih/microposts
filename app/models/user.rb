2cfae9448c527c86e5c9cbc7743fcd1b342fb591
masayuki.higashi <masayuki.h1109@gmail.com>
2017-12-31 02:30:14 +0000

    timeline

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
 	has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
 	has_many :followers, through: :reverses_of_relationship, source: :user
 	
+	def feed_microposts
+		Micropost.where(user_id: self.following_ids + [self.id])
+	end
+	
 	def follow(other_user)
    unless self == other_user
     self.relationships.find_or_create_by(follow_id: other_user.id)
