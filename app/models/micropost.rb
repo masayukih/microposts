9c296f6a77bac42517db04f690e6555a421080e8
masayuki.higashi <masayuki.h1109@gmail.com>
2018-01-15 14:29:56 +0000

    kadai

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,9 @@
 class Micropost < ApplicationRecord
-  belongs_to :user
-  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 255 }
+
+  belongs_to :user
+
+  has_many :reverses_of_favorite
+  has_many :favorite_users, through: :reverses_of_favorite, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ApplicationRecord
 	has_secure_password
 	
 	has_many :microposts
+	
 	has_many :relationships
 	has_many :followings, through: :relationships, source: :follow
 	has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
 	has_many :followers, through: :reverses_of_relationship, source: :user
+	
+	has_many :favorites
+	has_many :favorite_microposts, through: :favorites, source: :micropost
 	
 	def feed_microposts
 		Micropost.where(user_id: self.following_ids + [self.id])
@@ -29,5 +33,18 @@ class User < ApplicationRecord
   
   def following?(other_user)
    self.followings.include?(other_user)
+  end
+  
+  def favor(micropost)
+  	self.favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  def unfavor(micropost)
+  	favorite = self.favorites.find_by(micropost_id: micropost.id)
+  	favorite.destroy if favorite
+  end
+  
+  def favorite_micropost?(micropost)
+  	self.favorite_microposts.include?(micropost)
   end
 end
